1b7a4d3a2d99e5d7b38f3c60219ec4e2587a8cce
Olympia Mckee <mawekuwe@users.noreply.github.com>
2015-05-30 14:30:14 +0200

    bump version 1.1

--- Lichess_Chat_Enhancer.user.js
+++ Lichess_Chat_Enhancer.user.js
@@ -12,7 +12,7 @@
 // @updateURL   https://github.com/mawekuwe/UserScripts/raw/master/Lichess_Chat_Enhancer/Lichess_Chat_Enhancer.user.js
 // @supportURL  https://github.com/mawekuwe/UserScripts/issues
 // @contributionURL 
-// @version     1.0
+// @version     1.1
 // @grant       none
 // @run-at      document-end
 // @include     http://*.lichess.org/*
