e8ed894b3fac9b947c589af819d33ea57c45b391
Daniel Dugovic <dandydand@gmail.com>
2015-11-26 08:14:11 -0600

    Create Lichess_Gone_Berserk.user.js

---
+++ Lichess_Gone_Berserk.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @id          Lichess_Gone_Berserk@https://github.com/ddugovic/UserScripts
+// @name        Lichess Gone Berserk
+// @namespace   https://github.com/ddugovic/UserScripts
+// @description Go berserk whenever possible!
+// @author      ddugovic
+// @copyright   2015+, ddugovic
+// @license     CC0 1.0 Universal
+// @downloadURL https://raw.githubusercontent.com/ddugovic/UserScripts/master/Lichess_Gone_Berserk.user.js
+// @updateURL   https://raw.githubusercontent.com/ddugovic/UserScripts/master/Lichess_Gone_Berserk.user.js
+// @supportURL  https://github.com/ddugovic/UserScripts/issues
+// @version     0.1
+// @match       http://*.lichess.org/
+// @grant       none
+// @icon        https://raw.githubusercontent.com/ornicar/lila/master/public/images/favicon-32-black.png
+// ==/UserScript==
+
+setTimeout(function() {
+    $('button.berserk').click()
+}, 100)
