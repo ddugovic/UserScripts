1d63e53da1e0cf1971b38e4089d7dddc35dc9775
Daniel Dugovic <dandydand@gmail.com>
2019-03-31 20:46:11 -0500

    Replace create a game buttons with latest updates
    I have been struggling with having this script display elements above the seek buttons (in the center).
    Similarly I have struggled with many alternatives due to challenges overriding CSS rules.
    I don't use the buttons on the right side of the lobby and would like to see Latest Updates.

--- Lichess_Compact_Lobby.user.js
+++ Lichess_Compact_Lobby.user.js
@@ -9,36 +9,33 @@
 // @downloadURL https://raw.githubusercontent.com/ddugovic/UserScripts/master/Lichess_Compact_Lobby.user.js
 // @updateURL   https://raw.githubusercontent.com/ddugovic/UserScripts/master/Lichess_Compact_Lobby.user.js
 // @supportURL  https://github.com/ddugovic/UserScripts/issues
-// @version     0.6
+// @version     0.7
 // @match       https://lichess.org/
 // @grant       none
 // @icon        https://raw.githubusercontent.com/ornicar/lila/master/public/images/favicon-32-white.png
 // ==/UserScript==
 
-var container = document.getElementById('hooks_wrap');
+document.getElementById('daily_puzzle').style.display = 'none';
+
+var container = document.getElementById('start_buttons');
 var theOriginal = document.getElementsByClassName('blog')[0];
 var theClone = theOriginal.cloneNode(true);
-container.insertBefore(theClone, container.lastChild);
-theClone.style.marginTop = '0px';
-theClone.style.marginBottom = '15px';
+while (container.firstChild) {
+    container.firstChild.remove();
+}
+container.appendChild(theClone);
+theClone.style.margin = '0px';
+theClone.style.padding = '0px';
 theOriginal.parentNode.removeChild(theOriginal);
 
 theOriginal = document.getElementById('enterable_tournaments').parentNode;
-theClone = theOriginal.cloneNode(true);
-container.insertBefore(theClone, container.lastChild);
-theClone.style.marginTop = '0px';
-theClone.style.marginBottom = '15px';
 theOriginal.parentNode.removeChild(theOriginal);
 
 theOriginal = document.getElementById('enterable_simuls').parentNode;
-theClone = theOriginal.cloneNode(true);
-container.insertBefore(theClone, container.lastChild);
-theClone.style.marginTop = '0px';
-theClone.style.marginBottom = '15px';
 theOriginal.parentNode.removeChild(theOriginal);
 
-var leaderboard = document.getElementsByClassName('leaderboard')[0];
-leaderboard.parentNode.removeChild(leaderboard);
+var leaderboards = document.getElementsByClassName('leaderboards')[0];
+leaderboards.parentNode.removeChild(leaderboards);
 
 container = document.getElementById('site_header');
 theOriginal = document.getElementsByClassName('leaderboard')[0].parentNode;
