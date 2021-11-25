7f1af076663a98766c5f07cf2ba4f3c5bfa211ef
Daniel Dugovic <dandydand@gmail.com>
2021-11-25 07:57:43 -0600

    Update 81Dojo_Client_Volume.user.js

--- 81Dojo_Client_Volume.user.js
+++ 81Dojo_Client_Volume.user.js
@@ -6,8 +6,8 @@
 // @author       Daniel Dugovic
 // @copyright    2021+
 // @license      AGPL-3.0-or-later
-// @downloadURL  https://raw.githubusercontent.com/ddugovic/UserScripts/master/Lichess_Compact_Lobby.user.js
-// @updateURL    https://raw.githubusercontent.com/ddugovic/UserScripts/master/Lichess_Compact_Lobby.user.js
+// @downloadURL  https://raw.githubusercontent.com/ddugovic/UserScripts/master/81Dojo_Client_Volume.user.js
+// @updateURL    https://raw.githubusercontent.com/ddugovic/UserScripts/master/81Dojo_Client_Volume.user.js
 // @supportURL   https://github.com/ddugovic/UserScripts/issues
 // @match        http://81dojo.com/client/*
 // @grant        none
