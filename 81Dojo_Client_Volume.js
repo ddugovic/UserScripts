53c9d2577e2e48151121f253d5f046ac12fb8d78
Daniel Dugovic <dandydand@gmail.com>
2021-11-25 07:51:06 -0600

    Create 81Dojo_Client_Volume.js

---
+++ 81Dojo_Client_Volume.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         81Dojo Client Volume
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Reduce 81Dojo client volume
+// @author       Daniel Dugovic
+// @copyright    2021+
+// @license      AGPL-3.0-or-later
+// @match        http://81dojo.com/client/*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    options.master_volume = 0.7;
+})();
