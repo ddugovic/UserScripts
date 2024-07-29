f145d2d7b1c8f031c1765fcb806532898f81b3e9
Daniel Dugovic <dandydand@gmail.com>
2024-07-29 05:44:39 -0500

    Fix Lichess_Fridays_For_Future.user.js

--- Lichess_Fridays_For_Future.user.js
+++ Lichess_Fridays_For_Future.user.js
@@ -1,25 +1,17 @@
 // ==UserScript==
-// @id          Lichess_Fridays_For_Future@https://github.com/ddugovic/UserScripts
 // @name        Lichess Fridays For Future
-// @namespace   https://github.com/ddugovic/UserScripts
 // @description Fridays For Future - The World Needs You
-// @author      Thibault Duplessis
-// @copyright   2019+, ddugovic
-// @license     AGPL-3.0-or-later
-// @downloadURL https://raw.githubusercontent.com/ddugovic/UserScripts/master/Lichess_Fridays_For_Future.user.js
-// @updateURL   https://raw.githubusercontent.com/ddugovic/UserScripts/master/Lichess_Fridays_For_Future.user.js
-// @supportURL  https://github.com/ddugovic/UserScripts/issues
-// @version     0.1
 // @match       https://lichess.org/
+// @version     0.1
 // @grant       none
+// @run-at      document-idle
 // ==/UserScript==
 
 /* globals $ */
 $('body').append(
   '<div id="announce" class="fff">' +
   '<a class="link" href="https://fridaysforfuture.org">' +
-  '<img src="//lichess1.org/assets/images/fff.png" /> ' +
-  'Fridays For Future - The World Needs You</a>' +
+  '<img src="//lichess1.org/assets/images/flags/_earth.png" /> ' +
+  'Fridays For Future - The World Needs You</a>&nbsp;' +
   '<a class="close" title="Close">X</a>'
 ).find('#announce a').click(function() { $('#announce').remove(); });
-
